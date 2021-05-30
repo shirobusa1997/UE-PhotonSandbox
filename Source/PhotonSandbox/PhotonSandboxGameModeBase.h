8bb78974b7c858454eb801a5da4c636cb401fe4e
Mojatto <daichiandroid1997@gmail.com>
2021-05-31 08:29:46 +0900

    プロジェクトを新規に作成．

---
+++ Source/PhotonSandbox/PhotonSandboxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhotonSandboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHOTONSANDBOX_API APhotonSandboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
